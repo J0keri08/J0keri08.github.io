71c300b220703def6704e3f299417a4bbf539c52
J0keri08 <pascal@krausser-edv.de>
2025-12-10 21:51:45 +0100

    Full Responsive version

--- pages/includes/includes.js
+++
@@ -1,7 +0,0 @@
-    async function loadHTML(id, file) {
-      const res = await fetch(file);
-      const text = await res.text();
-      document.getElementById(id).innerHTML = text;
-    }
-    loadHTML("header", "/pages/includes/header.html");
-    loadHTML("footer", "/pages/includes/footer.html");
